73d3e603318e21b4931a4eb7b3ffe1573e43fa4a
acallejasz <5aadrianc@gmail.com>
2021-02-17 09:36:31 +0100

    Kafka_v1.0

---
+++ Kafka-ZooKeeper/Kafka/scripts_pruebas_multiple_broker/crear_productor.sh
@@ -0,0 +1,16 @@
+# Gestor de Flujos v.0 PLICA
+# Maintainer: Universidad Politecnica de Madrid
+# Contact: a.callejasz@upm.es
+
+
+#!/bin/bash
+
+# Creación de contenedor kafka pra hacer de productor
+# Se opta por el filtrado referenciado al nombre del contenedor
+# Se introduce manualmente el topic al que se quiere conectar
+# Cuando se ejecute el script indicar como primer parámetro el HOST_ADVERTISED_NAME (Que debe ser al gateway de la network definida en el compose) y como segundo el topic a usar de los creados
+
+broker_ip=$(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' kafka_PLICA)
+port=$(docker inspect kafka_kafka_2 | grep HostPort | sort | uniq | grep -o [0-9]*)
+
+docker exec -e HOST_IP=$1 -i -t kafka_kafka_2 /opt/kafka/bin/kafka-console-producer.sh --topic=$2 --bootstrap-server=$broker_ip:$port
