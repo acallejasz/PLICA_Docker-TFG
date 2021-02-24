b9bcb71950d6b4f38fa757ea3736499f066a7534
acallejasz <5aadrianc@gmail.com>
2021-02-24 19:07:26 +0100

    KAfka_SSL_v1.0

--- Kafka-ZooKeeper/Kafka/scripts_pruebas/crear_broker.sh
+++ Kafka-ZooKeeper/Kafka/scripts_pruebas/crear_broker.sh
@@ -5,13 +5,17 @@
 
 #!/bin/bash
 
-# Creación de contenedor kafka para hacer de consumidorr
-# Se introduce manualmente el topic al que se quiere conectar
-# Cuando se ejecute el script indicar como primer parámetro el HOST_ADVERTISED_NAME (Que debe ser al gateway de la network definida en el compose) y como segundo el topic a usar de los creados
+# Creación de contenedor kafka para hacer de broker
+# Se introducen manualmente los listener y puertos a utilizar, que nunca deben coincidir con el broker principal o con ningún otro broker
+# Por teclado se introduce: 
+#	- Primero el numero de puerto, que debe ser ser +3 el del último broker utilizado 
+#	- El segundo parámetro a introducir deber ser el id del broker, que no debe coincidir con ningún otro
 
 broker_ip=$(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' kafka_PLICA)
+port1=$(($1-1))
+port2=$(($1+1))
 
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v kafka_certs-zookeeper:/var/ssl/private/zookeeper -v kafka_certs-kafka:/var/ssl/private/kafka \
--v kafka_broker_kafka_config:/opt/kafka/config -e  KAFKA_ADVERTISED_HOST_NAME=$broker_ip -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2281 -e KAFKA_PORT=9096 -e KAFKA_ADVERTISED_PORT=9096 \
--e KAFKA_LISTENERS=PLAINTEXT://:9095,SSL://:9096,SASL_SSL://:9097 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://172.20.0.1:9095,SSL://172.20.0.1:9096,SASL_SSL://172.20.0.1:9097 \
--e  KAFKA_BROKER_ID=20 --name broker2 --network kafka_PLICA -p 9096:9096 -i -t acallejasz/kafka /usr/bin/start-broker.sh
+-v kafka_broker_kafka_config:/opt/kafka/config -e  KAFKA_ADVERTISED_HOST_NAME=$broker_ip -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2281 -e KAFKA_PORT=$1 -e KAFKA_ADVERTISED_PORT=$1 \
+-e KAFKA_LISTENERS=PLAINTEXT://:$port1,SSL://:$1,SASL_SSL://:$port2 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://$broker_ip:$port1,SSL://$broker_ip:$1,SASL_SSL://$broker_ip:$port2 \
+-e  KAFKA_BROKER_ID=$2 --name broker$2 --network kafka_PLICA -p $1:$1 -i -t acallejasz/kafka /usr/bin/start-broker.sh
