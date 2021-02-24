7ecf2e36888bdef27b715fbf28bf5715702782b8
acallejasz <5aadrianc@gmail.com>
2021-02-24 20:02:10 +0100

    Kafka_SSL_v1.0

--- Kafka-ZooKeeper/Kafka/scripts_pruebas/crear_productor.sh
+++ Kafka-ZooKeeper/Kafka/scripts_pruebas/crear_productor.sh
@@ -6,10 +6,9 @@
 #!/bin/bash
 
 # Creación de contenedor kafka para hacer de productor
-# Se introduce manualmente el topic al que se quiere conectar
-# Cuando se ejecute el script indicar como primer parámetro el HOST_ADVERTISED_NAME (Que debe ser al gateway de la network definida en el compose) y como segundo el topic a usar de los creados
+# Se introduce manualmente el topic al que se quiere conectar como primer y único parámetro
 
 broker_ip=$(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' kafka_PLICA)
 port=$(docker inspect kafka | grep HostPort | sort | uniq | grep -o [0-9]*)
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$broker_ip --name producer -i -t acallejasz/kafka /opt/kafka/bin/kafka-console-producer.sh --topic=$2 --bootstrap-server=$broker_ip:$port --producer.config /opt/kafka/config/producer_ssl.properties
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$broker_ip --name producer -i -t acallejasz/kafka /opt/kafka/bin/kafka-console-producer.sh --topic=$1 --bootstrap-server=$broker_ip:$port --producer.config /opt/kafka/config/producer_ssl.properties
