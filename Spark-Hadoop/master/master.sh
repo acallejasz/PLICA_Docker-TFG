e932bb4599a5f138aa1dce8467d48713486d749d
acallejasz <5aadrianc@gmail.com>
2021-03-31 14:34:28 +0200

    Modified_all_for_env_variables

--- Spark-Hadoop/master/master.sh
+++ Spark-Hadoop/master/master.sh
@@ -8,9 +8,9 @@ export SPARK_MASTER_HOST=`hostname`
 
 . "/spark/bin/load-spark-env.sh"
 
-mkdir -p $SPARK_MASTER_LOG
+mkdir -p ${SPARK_MASTER_LOG}
 
-ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
+ln -sf /dev/stdout ${SPARK_MASTER_LOG}/spark-master.out
 
 cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
-    --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
+    --ip ${SPARK_MASTER_HOST} --port ${SPARK_MASTER_PORT} --webui-port ${SPARK_MASTER_WEBUI_PORT} >> ${SPARK_MASTER_LOG}/spark-master.out
--- Spark-Hadoop/worker/worker.sh
+++ Spark-Hadoop/worker/worker.sh
@@ -6,9 +6,9 @@ export SPARK_HOME=/spark
 
 . "/spark/bin/load-spark-env.sh"
 
-mkdir -p $SPARK_WORKER_LOG
+mkdir -p ${SPARK_WORKER_LOG}
 
-ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
+ln -sf /dev/stdout ${SPARK_WORKER_LOG}/spark-worker.out
 
 /spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker \
-    --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
+    --webui-port ${SPARK_WORKER_WEBUI_PORT} ${SPARK_MASTER} >> ${SPARK_WORKER_LOG}/spark-worker.out
