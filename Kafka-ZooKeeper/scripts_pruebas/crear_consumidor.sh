8a84e12d13e16558dae960f75a96c4b0615372eb
acallejasz <5aadrianc@gmail.com>
2021-04-13 14:13:36 +0200

    Kafka v2.0

--- Kafka-ZooKeeper/scripts_pruebas/crear_consumidor.sh
+++ Kafka-ZooKeeper/scripts_pruebas/crear_consumidor.sh
@@ -5,11 +5,11 @@
 
 #!/bin/bash
 
-# Creación de contenedor kafka para hacer de productor
+# Creación de contenedor kafka para hacer de consumidor
 # Se introduce manualmente el topic al que se quiere conectar como primer y único parámetro
 
 broker_ip=$(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' kafka_PLICA)
 port=$(docker inspect kafka | grep HostPort | sort | uniq | grep -o [0-9]*)
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$broker_ip --name producer$2 -i -t acallejasz/kafka \
-/opt/kafka/bin/kafka-console-producer.sh --topic=$1 --bootstrap-server=$broker_ip:$port
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$broker_ip --name consumer -i -t kafka:2.7.0 \
+/opt/kafka/bin/kafka-console-consumer.sh --topic=$1 --from-beginning --bootstrap-server=$broker_ip:$port
