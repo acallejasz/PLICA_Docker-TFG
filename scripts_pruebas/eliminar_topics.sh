06cc1c5b5b3ee798845062f6abe9c8df6f7a3bc0
acallejasz <5aadrianc@gmail.com>
2021-04-13 16:19:09 +0200

    Start PLICA

---
+++ scripts_pruebas/eliminar_topics.sh
@@ -0,0 +1,27 @@
+# Gestor de Flujos v.0 PLICA
+# Maintainer: Universidad Politecnica de Madrid
+# Contact: a.callejasz@upm.es
+
+
+#!/bin/bash
+
+# NOTA: En docker (contenedor kafka) es necesario habilitar "delete.topics.enable=true" en el fichero
+# "server.properties" en el path del contenedor "/opt/kafka_2.12-0.11.0.1/config".
+
+# Filtrado por puertos expuestos, kafka comparte puerto con el conector de elastic
+# Se opta por el filtrado referenciado al nombre del contenedor levantado
+
+kafka_id=$(docker ps -q --filter "name=kafka")
+broker_ip=$(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' plica_KAFKA)
+port=$(docker inspect kafka | grep HostPort | sort | uniq | grep -o [0-9]*)
+
+declare -a topics=("WF-DATA" "BT-DATA" "CS-DATA" "RF-DATA" "RM-DATA" "TI-DATA" "PF-DATA" "UBA-DATA")
+
+for i in "${topics[@]}"
+do
+	# Eliminacion de topics desde Kafka instalado en docker
+	docker exec -itd $kafka_id /opt/kafka/bin/kafka-topics.sh --delete --zookeeper zookeeper:2181 --topic "$i"
+
+done
+
+echo Topics eliminados
