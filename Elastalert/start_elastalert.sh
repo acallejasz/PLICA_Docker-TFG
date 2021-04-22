7e20dd43ca662b6fb1118607c6198a5eb86c371c
acallejasz <5aadrianc@gmail.com>
2021-04-22 23:38:04 +0200

    Elastalert v1.0

---
+++ Elastalert/start_elastalert.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Start ssh server 
+
+/usr/sbin/sshd
+
+elastic_id=$(docker ps -q --filter "name=elasticsearch")
+
+if [ -z "${elastic_id}" ]; then
+	echo
+    echo "Error: Tienes que levantar primero el contenedor de Elasticsearch"
+    echo
+    exit 0
+fi
+
+sleep 4m
+
+elastalert-create-index --config /opt/config/config_correlacion_conjunta.yaml
+elastalert-create-index --config /opt/config/config_correlacion_individual.yaml
+elastalert --config /opt/config/config_correlacion_conjunta.yaml &
+elastalert --config /opt/config/config_correlacion_individual.yaml 
