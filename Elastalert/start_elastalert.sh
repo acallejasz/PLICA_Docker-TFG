b2878c8165c002939a4b78e920766eade1293828
acallejasz <5aadrianc@gmail.com>
2021-04-28 16:03:21 +0200

    Elastalert v1.0

--- Elastalert/start_elastalert.sh
+++ Elastalert/start_elastalert.sh
@@ -13,6 +13,18 @@ if [ -z "${elastic_id}" ]; then
     exit 0
 fi
 
+# Set required variables at /configFiles
+
+sed -i '17c\es_host: '"${ELASTICSEARCH_IP}" /opt/config/config_correlacion_conjunta.yaml
+sed -i '20c\es_port: '"${ELASTICSEARCH_PORT}" /opt/config/config_correlacion_conjunta.yaml
+sed -i '46c\es_username: '"${ELASTIC_USERNAME}" /opt/config/config_correlacion_conjunta.yaml
+sed -i '47c\es_password: '"${ELASTIC_PASSWORD}" /opt/config/config_correlacion_conjunta.yaml
+
+sed -i '17c\es_host: '"${ELASTICSEARCH_IP}" /opt/config/config_correlacion_individual.yaml
+sed -i '20c\es_port: '"${ELASTICSEARCH_PORT}" /opt/config/config_correlacion_individual.yaml
+sed -i '46c\es_username: '"${ELASTIC_USERNAME}" /opt/config/config_correlacion_individual.yaml
+sed -i '47c\es_password: '"${ELASTIC_PASSWORD}" /opt/config/config_correlacion_individual.yaml
+
 sleep 2m
 
 elastalert-create-index --config /opt/config/config_correlacion_conjunta.yaml
