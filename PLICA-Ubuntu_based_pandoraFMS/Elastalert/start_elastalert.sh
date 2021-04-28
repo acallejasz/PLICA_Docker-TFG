b1ebccb34fe7a77e9a8478c6ea5c3cbba9c68348
acallejasz <5aadrianc@gmail.com>
2021-04-28 16:02:40 +0200

    PLICA Ubuntu v1.0

--- PLICA-Ubuntu_based_pandoraFMS/Elastalert/start_elastalert.sh
+++ PLICA-Ubuntu_based_pandoraFMS/Elastalert/start_elastalert.sh
@@ -6,6 +6,18 @@
 
 service pandora_agent_daemon start
 
+# Set required variables at /configFiles
+
+sed -i '17c\es_host: '"${ELASTICSEARCH_IP}" /opt/config/config_correlacion_conjunta.yaml
+sed -i '20c\es_port: '"${ELASTICSEARCH_PORT}" /opt/config/config_correlacion_conjunta.yaml
+sed -i '46c\es_username: '"${ELASTIC_USERNAME}" /opt/config/config_correlacion_conjunta.yaml
+sed -i '47c\es_password: '"${ELASTIC_PASSWORD}" /opt/config/config_correlacion_conjunta.yaml
+
+sed -i '17c\es_host: '"${ELASTICSEARCH_IP}" /opt/config/config_correlacion_individual.yaml
+sed -i '20c\es_port: '"${ELASTICSEARCH_PORT}" /opt/config/config_correlacion_individual.yaml
+sed -i '46c\es_username: '"${ELASTIC_USERNAME}" /opt/config/config_correlacion_individual.yaml
+sed -i '47c\es_password: '"${ELASTIC_PASSWORD}" /opt/config/config_correlacion_individual.yaml
+
 sleep 1m
 
 elastalert-create-index --config /opt/config/config_correlacion_conjunta.yaml
