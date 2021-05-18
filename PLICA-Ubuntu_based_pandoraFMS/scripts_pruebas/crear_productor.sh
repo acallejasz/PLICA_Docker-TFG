982818be93391f96ee159af7beecca535f8d9de4
acallejasz <5aadrianc@gmail.com>
2021-05-18 17:55:31 +0200

    PLICA enviroment Ubuntu based with SSL v3.0

--- PLICA-Ubuntu_based_pandoraFMS/scripts_pruebas/crear_productor.sh
+++ PLICA-Ubuntu_based_pandoraFMS/scripts_pruebas/crear_productor.sh
@@ -12,4 +12,4 @@ broker_ip=$(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}
 port=$(docker inspect kafka | grep HostPort | sort | uniq | grep -o [0-9]*)
 
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$broker_ip --name producer$2 -i -t kafka:2.7.0 \
-/opt/kafka/bin/kafka-console-producer.sh --topic=$1 --bootstrap-server=$broker_ip:$port
+/opt/kafka/bin/kafka-console-producer.sh --topic=$1 --bootstrap-server=$broker_ip:9093 --producer.config /opt/kafka/config/producer_ssl.properties
